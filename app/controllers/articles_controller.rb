d590bc053b8e724ddb1fceb40343c62e21b65631
ryotahoshino <75008198+ryotahoshino@users.noreply.github.com>
2021-09-14 23:16:28 +0900

    コントローラーの編集

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,46 @@
 class ArticlesController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
   def index
+    @article = Article.order(created_at: :desc)
   end
 
   def show
   end
 
+  def new
+    @article = Article.new
+  end
+
   def edit
   end
 
-  def new
+  def create
+    @article = Article.new(post_params)
+    if @article.save
+      redirect_to @article, notice: '作成できました'
+    else
+      render :new, alert: '作成できませんでした'
+    end
+  end
+
+  def update
+    if @article.update(article_params)
+      redirect_to @article, notice: '更新できました'
+    else
+      render :edit, alert: '更新できませんでした'
+  end
+
+  def destroy
+    if @article.destroy
+      redirect_to root_path, notice: '削除に成功しました'
+    else
+      redirect_to root_psth, alert: '削除できませんでした'
+    end
+  end
+
+  private
+
+  def find_post
+    @article = Article.find(params[:id])
   end
 end
